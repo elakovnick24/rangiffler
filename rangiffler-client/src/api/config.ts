56ed102f525be69dda0ca47743d7607fe3d66537
nikitaelakov <nelakov24@gmail.com>
2023-07-19 21:58:17 +0400

    init commit

---
+++ rangiffler-client/src/api/config.ts
@@ -0,0 +1,5 @@
+export const FRONT_URL = process.env.REACT_APP_FRONT_URL;
+export const GATEWAY_URL = process.env.REACT_APP_GATEWAY_URL;
+export const AUTH_URL = process.env.REACT_APP_AUTH_URL;
+export const CLIENT = "client";
+export const SECRET = "secret";
