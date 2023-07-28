62b2f2329785548144562065a04442ba3974ae69
nikitaelakov <nelakov24@gmail.com>
2023-07-29 01:13:23 +0400

    feature-gateway-security:  - implement security configuration (it's not finished yet)

--- rangiffler-gateway/src/main/java/com/elakov/rangiffler/config/SecurityConfig.java
+++ rangiffler-gateway/src/main/java/com/elakov/rangiffler/config/SecurityConfig.java
@@ -1,31 +1,38 @@
 package com.elakov.rangiffler.config;
 
 import com.elakov.rangiffler.cors.CorsCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-
   private final CorsCustomizer corsCustomizer;
+  private final Environment environment;
 
-  public SecurityConfig(CorsCustomizer corsCustomizer) {
+  @Autowired
+  public SecurityConfig(CorsCustomizer corsCustomizer, Environment environment) {
     this.corsCustomizer = corsCustomizer;
+    this.environment = environment;
   }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     corsCustomizer.corsCustomizer(http);
 
-    http.authorizeHttpRequests(
-        authorize ->
-            authorize.anyRequest().permitAll()
-    ).csrf().disable();
+    http.csrf().disable().authorizeHttpRequests()
+            .requestMatchers("/actuator/health", "/favicon.ico").permitAll()
+            .requestMatchers(HttpMethod.POST).permitAll()
+            .anyRequest()
+            .authenticated().and()
+            .oauth2ResourceServer()
+            .jwt();
     return http.build();
   }
 }
