31d908d2e2cc59d1c4d69eaa1ac0eaee444ebf9a
nikitaelakov <nelakov24@gmail.com>
2023-07-29 22:33:21 +0400

    feature-gateway-security:  - update spring version to previous  - implement security configuration (it's not finished yet)

--- rangiffler-gateway/src/main/java/com/elakov/rangiffler/config/SecurityConfigMain.java
+++ rangiffler-gateway/src/main/java/com/elakov/rangiffler/config/SecurityConfigMain.java
@@ -4,20 +4,21 @@ import com.elakov.rangiffler.cors.CorsCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
-public class SecurityConfig {
+@Profile("!local")
+public class SecurityConfigMain {
   private final CorsCustomizer corsCustomizer;
   private final Environment environment;
 
   @Autowired
-  public SecurityConfig(CorsCustomizer corsCustomizer, Environment environment) {
+  public SecurityConfigMain(CorsCustomizer corsCustomizer, Environment environment) {
     this.corsCustomizer = corsCustomizer;
     this.environment = environment;
   }
@@ -25,16 +26,14 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     corsCustomizer.corsCustomizer(http);
-    // Конфигурируем сам Spring Security
-    // Конфигурируем авторизацию
+
     http.authorizeHttpRequests()
             .requestMatchers("/actuator/health").permitAll()
-            .requestMatchers(HttpMethod.POST).permitAll()
             .anyRequest()
-            .authenticated()
-            .and()
+            .authenticated().and()
             .oauth2ResourceServer()
             .jwt();
     return http.build();
   }
+
 }
